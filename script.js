4bc49d8022a312baef66e8b41d8be482b55400d7
Rajan gupta <81641542+Rajangupta9@users.noreply.github.com>
2025-03-17 12:28:17 +0530

    Update socket connection URL and adjust .gitignore formatting

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const socket = io("http://localhost:5000", { transports: ["websocket"] });
+const socket = io("https://realtimechatapp-5str.onrender.com", { transports: ["websocket"] });
 
 let currentUser = "";
 
